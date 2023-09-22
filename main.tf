c96d578ad451a20e732d2f1632b0bab7f6e51c03
Mohona-18 <80566776+Mohona-18@users.noreply.github.com>
2023-09-22 23:52:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2-instance" {
+    ami = "ami-0ccabb5f82d4c9af5"
+    instance_type = t2.micro
+
+	  key_name      = "win-demo-kp-1"
+	  tags= {
+	  Name= "Linux_Instance_Terraform"
+	  }
+	  
+}
