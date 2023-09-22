92ebdfd53bc0c08474ce7af7689a65006509f942
Mohona-18 <80566776+Mohona-18@users.noreply.github.com>
2023-09-23 00:23:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2-instance" {
     ami = "ami-0ccabb5f82d4c9af5"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
 
 	  key_name      = "win-demo-kp-1"
 	  tags= {
