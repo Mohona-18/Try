82e8cd300e009cfe9dac9d1acd615b8ffdddd648
Mohona-18 <80566776+Mohona-18@users.noreply.github.com>
2023-09-23 10:27:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIASLHNOVWYCVVSSOID"
+  secret_key = "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"
+}
 resource "aws_instance" "ec2-instance" {
     ami = "ami-0ccabb5f82d4c9af5"
     instance_type = "t2.micro"
